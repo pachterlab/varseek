873e0ba36cd5728bb4ff68ec9260de7b1b8522cc
josephrich98 <josephrich98@gmail.com>
2024-11-13 17:29:43 -0800

    changed to python >=3.9

--- setup_legacy.py
+++ setup_legacy.py
@@ -65,7 +65,6 @@ setup(
         "Intended Audience :: Science/Research",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
